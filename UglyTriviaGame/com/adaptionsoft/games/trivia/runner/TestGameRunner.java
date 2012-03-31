163d8b8b7f842319f3de0c74faf0ff7728581b10
Daniel Marquez <enchequito@gmail.com>
2012-03-31 17:54:46 +0200

    Version con alguna modificacion de la primera clase de Jordi Salvat

---
+++ UglyTriviaGame/com/adaptionsoft/games/trivia/runner/TestGameRunner.java
@@ -0,0 +1,14 @@
+package com.adaptionsoft.games.trivia.runner;
+
+import org.junit.Test;
+
+
+
+public class TestGameRunner {
+
+	@Test
+	public void testGame(){
+		GameRunner gameRunner = new GameRunner (); 
+		
+	}
+}
