b44820c6d71416b002fc5754ae271e8850577366
Daniel Marquez <enchequito@gmail.com>
2012-04-15 12:30:39 +0200

    quitar clase DAOGame que no se usa

--- UglyTriviaGame/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ UglyTriviaGame/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -21,7 +21,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.adaptionsoft.games.uglytrivia.DAOGamme;
 import com.adaptionsoft.games.uglytrivia.Game;
 import com.adaptionsoft.games.uglytrivia.Historic;
 import com.adaptionsoft.games.uglytrivia.Players;
