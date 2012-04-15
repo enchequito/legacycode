1354675845b6d79d1361498653f12aee3925f9bb
Daniel Marquez <enchequito@gmail.com>
2012-04-15 12:09:51 +0200

    Adaptar para realizar persistencia de historico de puntuaciones

--- UglyTriviaGame/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ UglyTriviaGame/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -3,11 +3,22 @@ package com.adaptionsoft.games.trivia.runner;
 import static org.junit.Assert.*;
 
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Random;
 
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.adaptionsoft.games.uglytrivia.DAOGamme;
@@ -23,6 +34,34 @@ public class GameRunnerTest {
 	private Game game;
 	private Random randomMock;
 	private static Players players;
+	private static Historic historic;
+	
+	@BeforeClass
+	public static void SetUpClass(){        
+		try{
+//			historic = new Historic();
+//	    	ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream("historic.obj"));
+//          salida.writeObject(historic);
+//          salida.close();
+			ObjectInputStream entrada=new ObjectInputStream(new FileInputStream("historic.obj"));
+	        historic=(Historic)entrada.readObject();
+	        historic.printResults();
+		}catch (FileNotFoundException ex){
+	    	System.out.println("No existe historico de puntuaciones, se crea");
+	    	try{
+	    		historic = new Historic();
+		    	ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream("historic.obj"));
+	            salida.writeObject(historic);
+	            salida.close();
+	    	}catch (IOException exc) {
+		        System.out.println(exc);	    
+		    }
+		}catch (IOException ex) {
+	        System.out.println(ex);
+	    }catch (ClassNotFoundException ex) {
+	        System.out.println(ex);	    
+	    }
+	}
 	
 	@Before
 	public void SetUp(){
@@ -31,6 +70,18 @@ public class GameRunnerTest {
 		players = new Players();
 		outContent = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(outContent));
+	}
+	
+	@AfterClass
+	public static void afterExecution(){
+		historic.printResults();
+		try{
+	    	ObjectOutputStream salida=new ObjectOutputStream(new FileOutputStream("historic.obj"));
+            salida.writeObject(historic);
+            salida.close();
+    	}catch (IOException exc) {
+	        System.out.println(exc);	    
+	    }
 	}
 		
 	@Test
@@ -108,5 +159,35 @@ public class GameRunnerTest {
 			players.addPlayer("Player".concat(String.valueOf(player)));
 		}
 		return players;
-	}		
+	}
+	
+	@Test
+	public void validate_scores_one_play_for_player_one_and_all_answers_ok_has_6_gold_coins() {				
+		when(randomMock.nextInt(5)).thenReturn(0);
+		when(randomMock.nextInt(9)).thenReturn(2);
+		
+		game = new Game();
+		game.setPlayers(setUpPlayersToPlay(1));
+		GameRunner.setTotalRoundsToPlay(6);
+		GameRunner.play(game, randomMock);
+
+		historic.addPlayersScores(game.getPlayersScores());
+		
+		assertEquals(6,game.getPlayersScores().get("Player1"));
+	}
+
+	@Test
+	public void validate_scores_game_with_3_players_some_answers_ok_and_player_three_has_6_gold_coins() {
+		Game gameSpy = spy(new Game());
+		gameSpy.resetPlayers();
+		Random randomSpy = spy (new Random());
+		when(randomSpy.nextInt(9)).thenReturn(7).thenReturn(7).thenReturn(1);
+		
+		gameSpy.setPlayers(setUpPlayersToPlay(3));
+		GameRunner.play(gameSpy, randomSpy);				
+
+		historic.addPlayersScores(gameSpy.getPlayersScores());
+		assertNotNull(gameSpy.getPlayersScores());
+	}
+
 }
